78c6c789c655a1db1137e4c1aa05f574e6125549
Mahesh <thumarmahesh@gmail.com>
2020-12-02 09:59:31 +0530

    misc.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from 'apollo/client'
 
-export default function App({ Component, pageProps }) {
+
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+
+export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
