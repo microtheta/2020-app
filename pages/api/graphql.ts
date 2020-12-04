fd451e1c67a0bc5bd7f5f073258b7a11793c0aa1
Mahesh <thumarmahesh@gmail.com>
2020-12-04 10:12:54 +0530

    misc upgrade

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { schema } from 'apollo/schema'
 import { db } from 'utils/db'
+import packageConfig from 'package.json'
 
 const apolloServer = new ApolloServer({
   schema,
@@ -11,6 +12,12 @@ const apolloServer = new ApolloServer({
     req: event ? event : req,
     context,
   }),
+  formatResponse: (response: any, requestContext: any) => {
+    if (requestContext.response && requestContext.response.http) {
+      requestContext.response.http.headers.set('package-version', packageConfig.version);
+    }
+    return response;
+  },
   playground: {
     endpoint: "/api/graphql"
   }
