9696c4fb653d9bed9064d06d6f4226c1efa4b242
Mahesh <thumarmahesh@gmail.com>
2020-12-25 08:55:13 +0530

    misc

--- apollo/resolvers/queryResolver.js
+++ apollo/resolvers/queryResolver.js
@@ -5,8 +5,8 @@ export default {
     Query: {
         viewer: async (_parent, _args, _context, _info) => {
             const dbUser = await db.users.findFirst()
-            const { id } = dbUser;
-            return { id, name: 'John Smith', status: 'cached-1', address: {} }
+            const { id, name } = dbUser;
+            return { id, name, status: 'cached-1', address: {} }
         },
     },
 }
